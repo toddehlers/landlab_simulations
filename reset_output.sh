0250ba1108ef08f93917bf33c130f6b72782e988
willi <willi.kappler@uni-tuebingen.de>
2019-05-07 11:44:31 +0200

    add reset script

---
+++ reset_output.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+rm ACC/*
+rm CSVOutput/*
+rm DEM/*
+rm DHDT/*
+rm Ksn/*
+rm NC/*
+rm SA/*
+rm SoilDepth/*
+rm dd/*
+
+mkdir ACC
+mkdir CSVOutput
+mkdir DEM
+mkdir DHDT
+mkdir Ksn
+mkdir NC
+mkdir SA
+mkdir SoilDepth
+mkdir dd
+
