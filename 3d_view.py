172771ecccc4867f7b299079ac728d03aa41f2cf
Willi Kappler <willi.kappler@uni-tuebingen.de>
2019-05-13 16:21:35 +0200

    Plot script for 3D view of topography

---
+++ 3d_view.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+
+from netCDF4 import Dataset
+
+import numpy as np
+
+font_color = 'white'
+bg_color = 'black'
+
+mpl.rcParams['text.color'] = font_color
+mpl.rcParams['axes.labelcolor'] = font_color
+mpl.rcParams['xtick.color'] = font_color
+mpl.rcParams['ytick.color'] = font_color
+mpl.rcParams['savefig.facecolor'] = bg_color
+
+# mpl.rcParams['ztick.color'] = font_color
+
+
+def process(filename):
+    print('processing file: {}'.format(filename))
+    basefile, extension = os.path.splitext(filename)
+
+    nc_file = Dataset(filename, mode='r')
+
+    scale_factor = 0.001
+
+    x = nc_file.variables['x'][:] * scale_factor
+    y = nc_file.variables['y'][:] * scale_factor
+    z = nc_file.variables['topographic__elevation'][:][0] * scale_factor
+
+    # print('x: {}, y: {}, z: {}'.format(len(x), len(y), len(z)))
+
+    z_min = 0.0
+    z_max = 0.8 # Azucar
+    z_max = 2.0 # Nahuelbuta
+
+    num_of_vals = 100
+
+    fig = plt.figure(figsize=(10, 7))
+    fig.patch.set_facecolor(bg_color)
+    fig.set_facecolor(bg_color)
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_zlim(0.0, 10.0)
+    ax.view_init(50, 45)
+    ax.set_xlabel('X [km]')
+    ax.set_ylabel('Y [km]')
+    ax.set_zlabel('Z [km]')
+    ax.grid(False)
+    ax.xaxis.pane.set_edgecolor(bg_color)
+    ax.yaxis.pane.set_edgecolor(bg_color)
+    ax.zaxis.pane.set_edgecolor(bg_color)
+    ax.xaxis.pane.fill = False
+    ax.yaxis.pane.fill = False
+    ax.zaxis.pane.fill = False
+    ax.set_facecolor(bg_color)
+    surface = ax.plot_surface(x, y, z, cmap=cm.terrain, linewidth=0, antialiased=True, vmin=z_min, vmax=z_max, rcount=num_of_vals, ccount=num_of_vals)
+    cbar = fig.colorbar(surface, label='Elevation [km]')
+    plt.savefig('{}.png'.format(basefile), dpi=100, bbox_inches='tight')
+    plt.close()
+
+for filename in sys.argv[1:]:
+    process(filename)
+
